6146d333ef58a7523c755c0d08e6462105112b61
Sami Leon <samileon@hotmail.fr>
2024-03-16 14:30:21 -0400

    Simpliying debias_plmm function

--- R/posi.R
+++ R/posi.R
@@ -1,105 +1,71 @@
-library(dplyr)
+debias_plmm <- function(data, plmm_output, a = 1, Z = NULL) {
+  x <- as.matrix(subset(data, select = -c(series, position, y)))
+  y <- data$y - plmm_output$Res.F$out.F$F.fit
 
-debias.plmm <- function(simu, model, a = 1, level = 0.95, Z = NULL) {
-  data.lmm <- simu
-  data.lmm$Y <- data.lmm$Y - model$Res.F$out.F$F.fit
-  
-  X <- as.matrix(subset(data.lmm, select = -c(series, position, Y)))
-  y <- data.lmm$Y
-  
-  grp <- factor(data.lmm$series)
-  
-  z <- model.matrix(~ as.factor(series) - 1, data.lmm[, "series", drop = F])
-  
-  Sigma <- a * model$su * z %*% t(z) + model$se * diag(rep(1, length(y)))
-  
-  Sigma.svd <- svd(Sigma)
-  Sig.a.inv.half <- Sigma.svd$u %*% diag(1 / sqrt(Sigma.svd$d)) %*% t(Sigma.svd$u)
-  
-  X.a <- Sig.a.inv.half %*% X
-  y.a <- Sig.a.inv.half %*% y
-  
-  if(is.null(Z)) {
-    X.a <- X.a[, colnames(X.a) %in% c(
-      names(model$Res.F$theta[-1][model$Res.F$theta[-1] != 0]),
-      sample(names(model$Res.F$theta[-1][model$Res.F$theta[-1] == 0]), 5)
-    )]
-    
-    de.sparsified.sl <- hdi::lasso.proj(X.a, y.a,
-                                        suppress.grouptesting = TRUE, return.Z = T,
-                                        do.ZnZ = T, betainit = "scaled lasso"
+  grp <- factor(data$series)
+
+  z <- model.matrix(~ as.factor(series) - 1, data[, "series", drop = F])
+
+  Sigma_a <- a * plmm_output$su * z %*% t(z) + plmm_output$se * diag(rep(1, length(y)))
+
+  Sigma_a_svd <- svd(Sigma_a)
+  Sigma_a_sqrt_inv <- Sigma_a_svd$u %*% diag(1 / sqrt(Sigma_a_svd$d)) %*% t(Sigma_a_svd$u)
+
+  x_a <- Sigma_a_sqrt_inv %*% x
+  y_a <- Sigma_a_sqrt_inv %*% y
+
+  if (is.null(Z)) {
+    de_sparsified <- hdi::lasso.proj(x_a, y_a,
+      suppress.grouptesting = TRUE, return.Z = TRUE,
+      do.ZnZ = TRUE, betainit = "scaled lasso"
     )
-    
-    lp.Z <- de.sparsified.sl$Z
+
+    debias_score_matrix <- de_sparsified$Z
   } else {
-    lp.Z = Z
+    debias_score_matrix <- Z
   }
-  
-  
-  N <- length(y)
-  n <- length(unique(grp))
-  q <- ncol(z)
-  p <- ncol(X)
-  
-  beta.db.mlm <- NULL
-  beta.db.sd.mlm <- NULL
-  
-  beta.hat <- model$Res.F$theta[names(model$Res.F$theta) %in% colnames(X.a)]
-  
-  res <- y.a - X.a %*% beta.hat
-  
-  for (j in 1:length(beta.hat)) {
-    col.j <- j
+
+  beta_original <- plmm_output$Res.F$theta[-1] # removing intercept
+
+  res <- y_a - x_a %*% beta_original
+
+  p <- length(beta_original)
+
+  beta_debias <- rep(NA, p)
+  beta_debias_sd <- rep(NA, p)
+
+  for (j in 1:p) {
+    score_j <- debias_score_matrix[, j]
+
+    beta_debias[[j]] <- beta_original[j] + sum(score_j * res) / sum(score_j * x_a[, j])
+
+    # Group by 'grp' and summarize scaled_res
+    df_res <- dplyr::summarize(dplyr::group_by(data.frame(score_j, res, grp), grp),
+                        scaled_res = (sum(score_j * res))^2)
     
-    wj.mlm <- lp.Z[, j]
-    
-    beta.db.mlm[j] <- beta.hat[col.j] + sum(wj.mlm * res) / sum(wj.mlm * X.a[, col.j])
-    
-    ok <- data.frame(wj.mlm, res, grp) %>%
-      group_by(grp) %>%
-      summarize(num = (sum(wj.mlm * res))^2)
-    num <- sum(ok$num)
-    
-    beta.db.sd.mlm[j] <- sqrt(num) / sqrt((sum(wj.mlm * X.a[, col.j]))^2)
+    # Calculate scaled_rss
+    scaled_rss <- sum(df_res$scaled_res)
+
+    beta_debias_sd[[j]] <- sqrt(scaled_rss) / sqrt((sum(score_j * x_a[, j]))^2)
   }
-  
-  if (level == 0.95) {
-    ci <- cbind(
-      beta.db.mlm - 1.96 * beta.db.sd.mlm,
-      beta.db.mlm + 1.96 * beta.db.sd.mlm
-    )
-  }
-  
-  if (level == 0.90) {
-    ci <- cbind(
-      beta.db.mlm - 1.64 * beta.db.sd.mlm,
-      beta.db.mlm + 1.64 * beta.db.sd.mlm
-    )
-  }
-  
-  if (level == 0.99) {
-    ci <- cbind(
-      beta.db.mlm - 2.58 * beta.db.sd.mlm,
-      beta.db.mlm + 2.58 * beta.db.sd.mlm
-    )
-  }
-  
-  # Calculate p-value
-  bprojrescaled <- beta.db.mlm * (1 / beta.db.sd.mlm)
-  pv <- 2 * pnorm(abs(bprojrescaled), lower.tail = FALSE)
-  
-  names(beta.db.mlm) <- colnames(X.a)
-  
-  debias.PLMM <- list(
-    bhat = beta.db.mlm, bhat.sd = beta.db.sd.mlm, pv = pv,
-    ci = ci, beta.hat = beta.hat
+
+  ci <- cbind(
+    beta_debias - 1.96 * beta_debias_sd,
+    beta_debias + 1.96 * beta_debias_sd
   )
-  
-  df.posi = cbind(debias.PLMM$beta.hat, debias.PLMM$bhat, debias.PLMM$ci, debias.PLMM$pv)
-  colnames(df.posi) = c("Estimate", "Debiased", "CI lower", "CI upper", "p-value")
-  df.posi[, 1:4] <- round(df.posi[, 1:4], 2)
-  df.posi = as.data.frame(df.posi)
-  df.posi[,5] = roundpval(df.posi[,5])
-  
+
+  # Calculate p-value
+  beta_debias_rescaled <- beta_debias * (1 / beta_debias_sd)
+  pv <- 2 * pnorm(abs(beta_debias_rescaled), lower.tail = FALSE)
+
+  df.posi <- data.frame(
+    Estimate = beta_original,
+    Debiased = beta_debias,
+    `Std. Error` = beta_debias_sd,
+    `Lower 95%` = ci[, 1],
+    `Upper 95%` = ci[, 2],
+    `p-value` = pv, check.names = FALSE
+  )
+
   return(df.posi)
 }
