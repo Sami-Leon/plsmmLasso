d47c30c94c67d8bb75d472903a6e4aa4c02f04cd
Sami Leon <samileon@hotmail.fr>
2024-03-19 11:59:33 -0400

    Updated example to work with new functions

--- R/example.R
+++ R/example.R
@@ -7,17 +7,13 @@ data.sim = simulate_group_inter(N = 50, n_mvnorm = 3, grouped = TRUE,
 
 sim1 = data.sim$sim
 
-lambdas <- round(exp(seq(log(0.1), log(1 * 0.0001),
-                             length.out = 10
-)), digits = 4)
-
-gammas <- c(
-  0.000001, 0.0000001, 0.00000001, 0.000000001
-)
+# lambdas <- round(exp(seq(log(0.1), log(1 * 0.0001),
+#                              length.out = 10
+# )), digits = 4)
 # 
-# 
-# lambdas = c(0.0022, 0.0010)
-# gammas <- c(0.0000001, 0.00000001)
+# gammas <- c(
+#   0.000001, 0.0000001, 0.00000001, 0.000000001
+# )
 
 lambdas = c(0.0046, 0.0001)
 gammas <- 0.00000001
@@ -30,15 +26,10 @@ t = sim1$position
 bases = create_bases(t, keep = NULL)
 
 
-# lasso_sim1 = plmm_lasso(x, y, series, t, name_group_var = "group", bases$bases,
-#                         gamma = gammas[1], lambda = lambdas[1], timexgroup = TRUE,
-#                         criterion = "BIC")
-# 
-# name_group_var = "group"
-# gamma = gammas[2]
-# lambda = lambdas[3]
-# timexgroup = TRUE
-# criterion = "BIC"
+lasso_sim1 = plmm_lasso(x, y, series, t, name_group_var = "group", bases$bases,
+                        gamma = gammas[1], lambda = lambdas[1], timexgroup = TRUE,
+                        criterion = "BIC")
+
 
 # Run model on a grid of hyperparameter and retrieve the model with the best BIC
 tuned_plmm <- tune_plmm(x, y, series, t, name_group_var = "group", bases$bases,
@@ -58,11 +49,14 @@ posi
 
 
 # Inference for the nonlinear functions
-test.nonlinear.functions = f.test(data.sim[[1]], fit.plmm, n = 1000)
+test_nonlinear_functions = test_f(x, y, series, t,  name_group_var = "group",
+                                  tuned_plmm, n_boot = 10, predicted = F)
 
 # Test of equality of the functions H0: f1 = f2
-test.nonlinear.functions[[1]]
+test_nonlinear_functions[[1]]
+
+head(test_nonlinear_functions[[2]])
 
 # Continuous joint confidence bands for the difference between f1 and f2
-test.nonlinear.functions[[3]]
+test_nonlinear_functions[[3]]
 
