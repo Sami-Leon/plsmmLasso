98546897d0d00056f6e555a834f57acbe36d69e3
Sami Leon <samileon@hotmail.fr>
2024-03-18 22:15:34 -0400

    Updated example

--- R/example.R
+++ R/example.R
@@ -1,38 +1,59 @@
 # Generating simulated dataset
-data.sim = simulate_group_inter(N = 100, n_mvnorm = 100, grouped = TRUE,
+set.seed(12)
+data.sim = simulate_group_inter(N = 50, n_mvnorm = 3, grouped = TRUE,
                                 timepoints = 3:5, nonpara_inter = TRUE,
                                 sample_from = seq(0,52,13), cst_ni = FALSE, 
                                 cos = FALSE, A_vec = c(1, 1.5))
 
+sim1 = data.sim$sim
 
-lambda.grid <- round(exp(seq(log(0.1), log(1 * 0.0001),
+lambdas <- round(exp(seq(log(0.1), log(1 * 0.0001),
                              length.out = 10
 )), digits = 4)
 
-gamma.grid <- c(
+gammas <- c(
   0.000001, 0.0000001, 0.00000001, 0.000000001
 )
+# 
+# 
+# lambdas = c(0.0022, 0.0010)
+# gammas <- c(0.0000001, 0.00000001)
+
+lambdas = c(0.0046, 0.0001)
+gammas <- 0.00000001
+
+x = as.matrix(sim1[,-1:-3])
+y = sim1$y
+series = sim1$series
+t = sim1$position
+
+bases = create_bases(t, keep = NULL)
+
+
+# lasso_sim1 = plmm_lasso(x, y, series, t, name_group_var = "group", bases$bases,
+#                         gamma = gammas[1], lambda = lambdas[1], timexgroup = TRUE,
+#                         criterion = "BIC")
+# 
+# name_group_var = "group"
+# gamma = gammas[2]
+# lambda = lambdas[3]
+# timexgroup = TRUE
+# criterion = "BIC"
 
 # Run model on a grid of hyperparameter and retrieve the model with the best BIC
-fit.plmm <- select.plmm(
-  data = data.sim[[1]], gamma = gamma.grid, lambda = lambda.grid,
-  intercept = T, timexgroup = T
-)
+tuned_plmm <- tune_plmm(x, y, series, t, name_group_var = "group", bases$bases,
+  gamma_vec = gammas, lambda_vec = lambdas, timexgroup = TRUE,
+  criterion = "BIC")
+
+tuned_plmm$hyperparameters
+tuned_plmm$lasso_output$theta
 
 # Visualize overall fit and nonlinear functions 
 plot.fit(fit.plmm, data.sim)
 
 # Get debiased fixed-effects and pvalues
-posi = debias.plmm(simu = data.sim[[1]], model = fit.plmm)
+posi = debias_plmm(x, y, series, tuned_plmm, a = 1, Z = NULL)
 posi
-
-colnames(data.sim[[1]])[3] = "y"
-
-posi = debias_plmm(x = as.matrix(subset(data.sim[[1]], select = -c(series, position, y))),
-                   y = data.sim[[1]]$y, series = data.sim[[1]]$series, 
-                   plmm_output = fit.plmm, a = 1, Z = NULL) 
-posi
-
 
 
 # Inference for the nonlinear functions
